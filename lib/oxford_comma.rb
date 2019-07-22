a956e4349d0f4261697053c3ca39fd0c5fb841bd
Christopher Dent <christopherdent01@gmail.com>
2019-07-22 01:27:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ def oxford_comma(array)
     array.insert(-2, 'and')
     string = array.join(", ")
     #need a method to remove part of a string, namely, the last comma in the string#
-    string.gsub(/:|\./, "")
+    string.gsub(/and,/, "and")
  end
 
 end
